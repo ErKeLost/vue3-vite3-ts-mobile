17f8a3fca8edba6e57422f19236c653429cfe131
ERKELSOT <1256029807@qq.com>
2022-09-12 10:47:58 +0800

    chore(postcss): update config

--- vw.postcss.config.cjs
+++ vw.postcss.config.cjs
@@ -1,4 +1,14 @@
-// postcss.config.cjs
+/**
+ * 由于在vite中用 module.exports = (param) => {} 这种方式导出postcss配置时，param中没有文件相关信息，
+ * 同时postcss-px-to-viewport也没有提供类似postcss-pxtorem中 rootValue({ file }) {} 的方法，无法根据文件路径动态设置viewportWidth
+ * 所以只能通过多次px2viewport()处理不同文件的hack方式来设置viewportWidth
+ *
+ * postcss-px-to-viewport v1.1.1不支持include配置项，v1.2.0开始加入include，但是并没有发布到npm仓库
+ * 如在v1.1.1中使用include，无效果，并且执行多次导致转换混乱
+ *
+ * postcss-px-to-viewport 不支持 postcss 8.x，而vite内置postcss 8.x，所以使用postcss-px-to-viewport会抛出警告
+ * 改用postcss-px-to-viewport-8-plugin替代
+ */
 
 const autoprefixer = require('autoprefixer')
 const px2viewport = require('postcss-px-to-viewport-8-plugin')
@@ -26,14 +36,14 @@ const basePx2viewport = {
 module.exports = {
   plugins: [
     autoprefixer(),
-    // vant
+    // 只将vant转为350设计稿的viewport
     px2viewport({
       ...basePx2viewport,
       viewportWidth: 375,
       exclude: [/^(?!.*node_modules\/vant)/]
       // include: [/node_modules\/vant/],
     }),
-    // 非vant
+    // 除了vant都转为750设计稿的viewport
     px2viewport({
       ...basePx2viewport,
       viewportWidth: 750,
