f9d6ff50c5a4aa9d6f4aa4eb1afdea64c3d96121
ERKELSOT <1256029807@qq.com>
2022-09-14 12:04:44 +0800

    chore(proxy): computed

--- src/views/city/cpns/search-city.vue
+++ src/views/city/cpns/search-city.vue
@@ -11,27 +11,46 @@ const cityStore = useCityStore()
 const { allCities } = storeToRefs(cityStore)
 const { fetchAllCitiesData } = cityStore
 fetchAllCitiesData()
+
+// 获取选中标签的数据
+const currentGroup = computed(() => {
+  return allCities.value[tabActive]
+})
 </script>
 
 <template>
   <div class="city top-page">
-    <van-search
-      v-model="value"
-      shape="round"
-      placeholder="城市 / 区域 / 位置"
-      show-action
-      @cancel="cancel"
-    ></van-search>
-    <van-tabs v-model:active="tabActive" color="#ff9854">
-      <template v-for="(value, key, index) in allCities" :key="key">
-        <van-tab :title="value.title"></van-tab>
+    <div class="top">
+      <van-search
+        v-model="value"
+        shape="round"
+        placeholder="城市 / 区域 / 位置"
+        show-action
+        @cancel="cancel"
+      ></van-search>
+      <van-tabs v-model:active="tabActive" color="#ff9854">
+        <template v-for="(value, key, index) in allCities" :key="key">
+          <van-tab :title="value.title" :name="key"></van-tab>
+        </template>
+      </van-tabs>
+      {{ currentGroup }}
+    </div>
+    <div class="content">
+      <template v-for="item in 100" :key="item">
+        <div>
+          {{ item }}
+        </div>
       </template>
-    </van-tabs>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .city {
   --van-tabs-line-height: 90px;
+}
+.content {
+  height: calc(100vh - 98px);
+  overflow-y: auto;
 }
 </style>
