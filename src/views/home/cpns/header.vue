33d70951cde3ce48cff914e3994dc790d33ed1c2
ERKELSOT <1256029807@qq.com>
2022-09-16 13:18:14 +0800

    chore(header): remove home title

--- src/views/home/cpns/header.vue
+++ src/views/home/cpns/header.vue
@@ -2,31 +2,15 @@
 
 <template>
   <div class="location-container">
-    <div class="nav-bar">
-      <div class="title">安东尼娅</div>
-    </div>
     <div class="banner">
       <img src="@/assets/img/home/banner.webp" alt="" />
     </div>
   </div>
 </template>
 <style scoped lang="scss">
-.home {
-  .nav-bar {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100px;
-    .title {
-      color: var(--primary-color);
-      font-size: 32px;
-      font-weight: 600;
-    }
-  }
-}
 .banner {
   img {
-    width: 100%;
+    width: 100vw;
   }
 }
 </style>
